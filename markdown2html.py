ff8f136e9c34d345df03d1a39e357d331c650073
Fpierr <fanuelp9@gmail.com>
2024-08-14 22:45:48 +0200

    add mark down

---
+++ markdown2html.py
@@ -0,0 +1,18 @@
+import sys
+import os
+
+def main():
+    if len(sys.argv) != 3:
+        print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
+        sys.exit(1)
+
+    markdown_file = sys.argv[1]
+
+    if not os.path.exists(markdown_file):
+        print("Missing {}".format(markdown_file), file=sys.stderr)
+        sys.exit(1)
+
+    sys.exit(0)
+
+if __name__ == "__main__":
+    main()
