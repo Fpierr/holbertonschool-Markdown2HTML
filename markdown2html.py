df8a39f2e7151436f32a818a6e1e47f934267fff
Fpierr <fanuelp9@gmail.com>
2024-08-14 23:02:05 +0200

    add mark down

--- markdown2html.py
+++ markdown2html.py
@@ -1,15 +1,22 @@
+#!/usr/bin/python3
+"""
+Module to convert Markdown to HTML
+"""
+
 import sys
 import os
 
 def main():
+    """Main function that processes the Markdown to HTML conversion."""
     if len(sys.argv) != 3:
         print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
         sys.exit(1)
 
     markdown_file = sys.argv[1]
+    html_file = sys.argv[2]
 
     if not os.path.exists(markdown_file):
-        print("Missing {}".format(markdown_file), file=sys.stderr)
+        print(f"Missing {markdown_file}", file=sys.stderr)
         sys.exit(1)
 
     sys.exit(0)
